36c5b5b3ca34aef122d143dd9b649c9bd47803dd
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-11-27 15:11:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,4 @@
 
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
-  def initialize(brand, color, size, material, condition)
-    @brand = brand
-    @color = color
-    @size = size
-    @material = material
-    @condition = condition
-  end
 end
